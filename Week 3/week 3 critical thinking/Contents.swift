06f7e68c12ec63fe3b9f6e120df10fd28aa045f6
mattkeyzer <105663113+mattkeyzer@users.noreply.github.com>
2023-02-12 16:32:50 -0600

    Add files via upload

---
+++ Week 3/week 3 critical thinking/Contents.swift
@@ -0,0 +1,31 @@
+//variables
+let q1 = "what is the plural form of octopus?"
+let a1 = "pdupqvtft"
+let q2 = "how many hearts does an octopus have?"
+let a2 = "uissf"
+let q3 = "how many brains doess an octopus have?"
+let a3 = "ojof"
+
+//quiz introduction
+print("welcome to matt's octopus quiz!")
+print("goodluck! \n")
+print("to crack the answer and see if you are correct follow this method!")
+
+print("use the Caesar Cipher with a shift of 1... for example, a -> b, e -> f, z -> a")
+
+print("if the answer is displayed as bqqmf... you guessed it, apple would be the right answer! \n\n")
+
+print("lets begin! \n")
+
+//question one
+print(q1)
+print(a1 + "\n")
+
+//question two
+print(q2)
+print(a2 + "\n")
+
+//question three
+print(q3)
+print(a3 + "\n")
+

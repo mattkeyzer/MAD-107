85d090605e80a23e4019004f58f79586271fd2b8
mattkeyzer <105663113+mattkeyzer@users.noreply.github.com>
2023-02-15 20:55:53 -0600

    Add files via upload

---
+++ OperatorExerciseMattK.playground/Contents.swift
@@ -0,0 +1,39 @@
+// *** variables ***
+let x = 3.0
+let y = 10.0
+
+
+// *** processing ***
+
+// addition
+var add = x + y
+
+// subtraction
+var subtract = x - y
+
+// multiplication
+var multiply = x * y
+
+// division
+var divide = x / y
+
+// percentage
+var percent = x / y * 100
+
+
+// *** output ***
+
+// addition
+print("3 + 10 = \(add)")
+
+// subtraction
+print("3 - 10 = \(subtract)")
+
+// multiplication
+print("3 * 10 = \(multiply)")
+
+// division
+print("3 / 10 = \(divide)")
+
+//percentage
+print("3 is \(percent) percent of 10")

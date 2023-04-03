e01d1ec75741c1a2ac048a34484e2be46901950e
mattkeyzer <105663113+mattkeyzer@users.noreply.github.com>
2023-04-02 22:26:02 -0500

    Add files via upload
    initial commit

---
+++ woofwalker/woofwalker/woofwalkerApp.swift
@@ -0,0 +1,17 @@
+//
+//  woofwalkerApp.swift
+//  woofwalker
+//
+//  Created by Matt Keyzer on 4/2/23.
+//
+
+import SwiftUI
+
+@main
+struct woofwalkerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
